8f6ce134d58a8e326d43e34d5c392fc949e2c666
djumanov <djumanovdev@gmail.com>
2026-05-21 19:13:55 +0500

    class based view in drf

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 from django.contrib.auth import get_user_model
@@ -6,6 +7,5 @@ User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_verified')
-    
+class UserAdmin(BaseUserAdmin):
+    pass
--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
-from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+class UserCreateAPIView(APIView):
+    def post(self, request: Request) -> Response:
+        # Implement user creation logic here
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
 
+    def get(self, request: Request) -> Response:
+        return Response({"message": "GET method is not allowed for this endpoint"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
