e0fe56d23379954074769c95521e44cee5c13f8e
Akbar Fayzullayev <ozodbekfayzullayev1220@gmail.com>
2026-05-23 22:56:48 +0500

    updated

--- django-custom-user-model/users/views.py
+++ django-custom-user-model/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializer import UserSerializer
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
