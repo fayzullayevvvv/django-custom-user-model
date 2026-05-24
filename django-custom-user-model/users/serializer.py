4402c77ddc1e8f27fda98a355d84e77a12c7e101
fayzullayevvvv <ozodbekfayzullayev1220@gmail.com>
2026-05-24 13:29:34 +0500

    updated

--- django-custom-user-model/users/serializer.py
+++ django-custom-user-model/users/serializer.py
@@ -3,6 +3,51 @@ from rest_framework import serializers
 from .models import User, Todo
 
 
-class UserSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    description = serializers.
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "is_verified",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Todo
+        fields = [
+            "id",
+            "title",
+            "description",
+            "is_completed",
+            "user",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "user",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Title kamida 3 ta belgidan iborat bo'lishi kerak"
+            )
+        
+        return value
